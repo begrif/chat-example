88414df45972f5df87982edc45c1c0ede6599291
Benjamin Elijah Griffin <begrif-github@eli.users.panix.com>
2018-04-18 15:14:07 -0700

    Fixed timeout issues through wrapper functions.

--- client.pl
+++ client.pl
@@ -7,7 +7,9 @@ my ($timeout, $got_timeout);
 $remote  = shift || "localhost";
 $port    = shift || 2345;  # random port
 
+$got_timeout = 'timeout!';
 $timeout = 1;
+sub ALARM { die "$got_timeout\n" }
 
 if ($port =~ /\D/) { $port = getservbyname($port, "tcp") }
 die "No port" unless $port;
@@ -23,24 +25,32 @@ $| = 1;
 select(STDOUT);
 $| = 1;
 
-sub ALARM { $got_timeout = 1; }
-$SIG{ALRM} = \&ALARM;
-
 for ( ;; ) {
-  alarm $timeout;
 
-  $line = <SOCK>;
+  $line = read_with_timout(*SOCK);
   if (length($line)) {
       print $line;
-  } else {
-    alarm $timeout;
   }
 
-  $line = <STDIN>;
+  $line = read_with_timout(*STDIN);
   if (length($line)) {
       print SOCK $line;
   }
 }
 
 close (SOCK)                        || die "close: $!";
+
+# read wrapper that uses a timeout to avoid perpetual blocking
+sub read_with_timout {
+  my $fh = shift;
+  my $line;
+  eval {
+        local $SIG{ALRM} = \&ALARM;
+        alarm $timeout;
+        $line = <$fh>;
+        alarm 0;
+       };
+  if ($@ and $@ !~ /$got_timeout/) { logmsg("during socket read: $@\n")}
+  return $line;
+} # end &read_with_timout
 
--- server.pl
+++ server.pl
@@ -5,8 +5,8 @@ use Socket;
 use POSIX;
 use Carp;
 my $EOL = "\015\012";
-my $got_timeout = 0;
 my @chatters;
+my $got_timeout = 'timeout!';
 my $timeout = 1;
 
 sub logmsg { print "$0 $$: @_ at ", scalar localtime(), "\n" }
@@ -24,50 +24,52 @@ setsockopt(Server, SOL_SOCKET, SO_REUSEADDR, pack("l", 1))
 bind(Server, sockaddr_in($port, INADDR_ANY))    || die "bind: $!";
 listen(Server, SOMAXCONN)                       || die "listen: $!";
 
+# turn on autoflush on socket and STDOUT
+select(Server);
+$| = 1;
+select(STDOUT);
+$| = 1;
+
 logmsg "server started on port $port";
 
-my $paddr;
-sub ALARM { $got_timeout = 1; }
 # $SIG{CHLD} = \&REAPER;
-$SIG{ALRM} = \&ALARM;
-
+sub ALARM { die "$got_timeout\n" }
 
 for ( ;; ) {
-  alarm $timeout;
+  # new version of $paddr and Client every time through
+  my $paddr;
+  local *Client;
 
-  if ( $paddr = accept(Client, Server) ) {
-    select(Client);
-    $| = 1;
-    select(Server);
-    $| = 1;
-    select(STDOUT);
-
+  if ( $paddr = accept_with_timeout(*Client, *Server) ) {
     if( $! != EINTR ) {
-      my %client;
       my($port, $iaddr) = sockaddr_in($paddr);
       my $name = gethostbyaddr($iaddr, AF_INET);
 
-      %client = (
-	  port => $port,
-	  iaddr => $iaddr,
-	  paddr => $paddr,
-	  fh => *Client,
-	  user => undef,
-       );
+      # turn on autoflush for client
+      select(Client);
+      $| = 1;
+      select(STDOUT);
 
-      push(@chatters, \%client);
+      push(@chatters, {
+	    port   => $port,
+	    iaddr  => $iaddr,
+	    paddr  => $paddr,
+	    fh     => *Client,
+	    user   => undef,
+	    sender => undef,
+	  });
+
 
       logmsg "connection from $name [", inet_ntoa($iaddr), "] at port $port";
       print Client "Hello there. Your first entry should be your username$EOL";
-    } else {
-      # reset alarm because it went off
-      alarm $timeout;
     }
   } # if new client
+  
+  if ($@ and $@ !~ /$got_timeout/) { die "Issue in accept(): $@\n"; }
 
   for my $ch (@chatters) {
     my $handle = $$ch{fh};
-    my $in = <$handle>;
+    my $in = read_with_timout($handle);
     if (length($in)) {
        logmsg "got $$ch{port} message: $in";
        my $send;
@@ -75,23 +77,55 @@ for ( ;; ) {
        $in =~ s/^\s*//;
        $in =~ s/\s*$//;
 
-       if(!defined($$ch{user}) and length($in)) {
-          $$ch{user} = $in;
+       if(length($in)) {
+	 if(!defined($$ch{user})) {
+	   $$ch{user} = $in;
 
-	  $send = "User $in joined the chat$EOL";
-       } else {
-         $send = $in . $EOL;
-       }
+	   $send = "User $in joined the chat$EOL";
+	 } else {
+	   $send = $$ch{user} . ': ' . $in . $EOL;
+	 }
 
-       $$ch{sender} = 1;
-       for my $ls (@chatters) {
-         next if $$ls{sender};
-         my $outhandle = $$ls{fh};
-	 print $outhandle $send;
-       }
-       $$ch{sender} = undef;
+	 # send out message to all other listeners
+	 $$ch{sender} = 1;
+	 for my $ls (@chatters) {
+	   next if $$ls{sender};
+	   my $outhandle = $$ls{fh};
+	   print $outhandle $send;
+	 }
+	 $$ch{sender} = undef;
 
+      } # got something to share
     }
-  }
+  } # check for chat messages
 }
+
+# accept wrapper that uses a timeout to avoid perpetual blocking
+sub accept_with_timeout {
+  my $C = shift;
+  my $S = shift;
+  my $paddr;
+  eval {
+        local $SIG{ALRM} = \&ALARM;
+        alarm $timeout;
+        $paddr = accept($C, $S);
+        alarm 0;
+       };
+  if ($@ and $@ !~ /$got_timeout/) { logmsg("during accept: $@\n") }
+  return $paddr;
+} # end &accept_with_timeout
+
+# read wrapper that uses a timeout to avoid perpetual blocking
+sub read_with_timout {
+  my $fh = shift;
+  my $line;
+  eval {
+        local $SIG{ALRM} = \&ALARM;
+        alarm $timeout;
+        $line = <$fh>;
+        alarm 0;
+       };
+  if ($@ and $@ !~ /$got_timeout/) { logmsg("during socket read: $@\n")}
+  return $line;
+} # end &read_with_timout
 
